f94abee7ac1b405b4e0b6f86aa9b9b34bb48a1b1
Vitalii Obertynskyi <vitalii.obertynskyi@inmost.pro>
2024-09-24 11:18:30 +0300

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,55 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "IntellicheckSDK",
+    defaultLocalization: "en",
+    platforms: [
+        .iOS(.v17)
+    ],
+    products: [
+        .library(name: "IntellicheckSDK", targets: [
+            "IntellicheckFRMW",
+            "DocumentReader",
+            "DocumentReaderCore",
+            "RegulaCommon",
+            "Lottie"
+        ])
+    ],
+    targets: [
+//        .target(
+//            name: "Intellicheck",
+//            dependencies: [
+//                "IntellicheckFRMW",
+//                "DocumentReader",
+//                "DocumentReaderCore",
+//                "RegulaCommon",
+//                "Lottie"
+//            ],
+//            path: "Sources"
+//        ),
+        .binaryTarget(
+            name: "IntellicheckFRMW",
+            path: "./Sources/Intellicheck.xcframework"
+        ),
+        .binaryTarget(
+            name: "DocumentReader",
+            path: "./Sources/Frameworks/DocumentReader.xcframework"
+        ),
+        .binaryTarget(
+            name: "DocumentReaderCore",
+            path: "./Sources/Frameworks/DocumentReaderCore.xcframework"
+        ),
+        .binaryTarget(
+            name: "RegulaCommon",
+            path: "./Sources/Frameworks/RegulaCommon.xcframework"
+        ),
+        .binaryTarget(
+            name: "Lottie",
+            path: "./Sources/Frameworks/Lottie.xcframework"
+        )
+    ]
+)
+
---
+++ Sources/Intellicheck.xcframework/ios-arm64/Intellicheck.framework/Headers/Intellicheck.h
@@ -0,0 +1,18 @@
+//
+//  Intellicheck.h
+//  Intellicheck
+//
+//  Created by Vitalii Obertynskyi on 20.09.2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Intellicheck.
+FOUNDATION_EXPORT double IntellicheckVersionNumber;
+
+//! Project version string for Intellicheck.
+FOUNDATION_EXPORT const unsigned char IntellicheckVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Intellicheck/PublicHeader.h>
+
+#import <DocumentReader/DocumentReader.h>
