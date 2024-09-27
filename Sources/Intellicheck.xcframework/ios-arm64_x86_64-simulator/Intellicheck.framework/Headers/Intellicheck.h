e2b3d844858ce89377539161a672720cd47db5c3
Vitalii Obertynskyi <vitalii.obertynskyi@inmost.pro>
2024-09-27 13:14:10 +0300

    v 2.2

--- Sources/Intellicheck.xcframework/ios-arm64_x86_64-simulator/Intellicheck.framework/Headers/Intellicheck.h
+++ Sources/Intellicheck.xcframework/ios-arm64_x86_64-simulator/Intellicheck.framework/Headers/Intellicheck.h
@@ -14,5 +14,3 @@ FOUNDATION_EXPORT double IntellicheckVersionNumber;
 FOUNDATION_EXPORT const unsigned char IntellicheckVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <Intellicheck/PublicHeader.h>
-
-#import <DocumentReader/DocumentReader.h>
